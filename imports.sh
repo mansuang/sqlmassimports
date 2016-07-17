61c87452bc129800c9a80a9ac372fd0df473a8b7
Mansuang Pawong <mansaung@hotmail.com>
2016-07-18 05:36:26 +0700

    add comments

--- imports.sh
+++ imports.sh
@@ -2,8 +2,8 @@
 
 for entry in ./*.sql
 do
-  db=${entry%1900.sql*}
-  db=${db##*-}
+  db=${entry%1900.sql*} # text befor 1900.sql
+  db=${db##*-}		# text after last dash
   mysql --defaults-extra-file=./config.cnf -e "CREATE DATABASE backup_$db"
   mysql --defaults-extra-file=./config.cnf backup_$db < $entry
 done
